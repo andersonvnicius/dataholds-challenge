6fde957de081769cbbdcaada66f582e43cba3d56
and1 <andersonvnicius@gmail.com>
2021-07-14 11:15:06 -0300

    first commit

--- dags/dataholds_challenge/send_merged_csv_data_to_db_dag.py
+++ dags/dataholds_challenge/send_merged_csv_data_to_db_dag.py
@@ -5,9 +5,15 @@ from airflow.operators.python_operator import PythonOperator
 from psycopg2 import connect
 from datetime import datetime, timedelta
 
-from sql_create_tables import create_tables
-from sql_send_datasets import CopyCsvToPostgres
-from sql_merge import InsertMergedDataToTable
+# from dags.dataholds_challenge.lib import sql_create_tables, sql_send_datasets, sql_merge
+
+from dags.dataholds_challenge.lib.sql_create_tables import create_tables
+from dags.dataholds_challenge.lib.sql_merge import InsertMergedDataToTable
+from dags.dataholds_challenge.lib.sql_send_datasets import CopyCsvToPostgres
+
+# from lib/sql_create_tables import create_tables
+# from lib/sql_send_datasets import CopyCsvToPostgres
+# from lib/sql_merge import InsertMergedDataToTable
 
 default_args = {
     "owner": "Anderson",
@@ -34,11 +40,11 @@ pg_params = "host=127.0.0.1 dbname=postgres user=postgres password='1'"
 conn = connect(pg_params)
 
 
-def create_tables(ds, **kwargs):
+def create_tables_in_sql(ds, **kwargs):
     create_tables(conn)
 
 
-def copy_products_csv_to_db(ds, **kwargs):
+def copy_csv_files_to_db(ds, **kwargs):
     CopyCsvToPostgres(pg_params, csv_name="product_dataset", db_table_name="products").execute()
     CopyCsvToPostgres(pg_params, csv_name="category_dataset", db_table_name="categories").execute()
 
@@ -47,19 +53,19 @@ def insert_data_to_merged_table(ds, **kwargs):
     InsertMergedDataToTable(pg_params).execute()
 
 
-create_tables_task = PythonOperator(
-    task_id="create_tables_task",
+create_tables_in_sql_task = PythonOperator(
+    task_id="create_tables_in_sql_task",
     provide_context=True,
     depends_on_past=False,
-    python_callable=create_tables,
+    python_callable=create_tables_in_sql,
     dag=dag,
 )
 
-copy_csv_to_db_task = PythonOperator(
+copy_csv_files_to_db_task = PythonOperator(
     task_id="save_csv_on_db_task",
     provide_context=True,
     depends_on_past=False,
-    python_callable=copy_csv_to_db,
+    python_callable=copy_csv_files_to_db,
     dag=dag,
 )
 
@@ -71,4 +77,4 @@ insert_data_to_merged_table_task = PythonOperator(
     dag=dag,
 )
 
-create_tables_task >> copy_csv_to_db_task >> insert_data_to_merged_table_task
+create_tables_in_sql_task >> copy_csv_files_to_db_task >> insert_data_to_merged_table_task
